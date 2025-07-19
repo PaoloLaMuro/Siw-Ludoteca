f443d22819aadc99831e3a9553da344c85dce194
Passarini96 <vit.passarini@stud.uniroma3.it>
2025-07-19 16:52:04 +0200

    Aggiunto altro ENUM GENERE

---
+++ src/main/java/it/uniroma3/siw/model/Genere.java
@@ -0,0 +1,33 @@
+package it.uniroma3.siw.model;
+
+
+
+public enum Genere{
+    ACTION("Azione"),
+    ADVENTURE("Avventura"),
+    RPG("Gioco di ruolo"),
+    FPS("Sparatutto in prima persona"),
+    STRATEGY("Strategia"),
+    HORROR("Horror"),
+    PUZZLE("Rompicapo"),
+    SPORTS("Sportivo"),
+    RACING("Corsa"),
+    SIMULATION("Simulazione"),
+    PLATFORM("Platform"),
+    MMO("Massively Multiplayer Online");
+
+    private final String descrizione;
+
+    Genere(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    @Override
+    public String toString() {
+        return descrizione;
+    }
+}
