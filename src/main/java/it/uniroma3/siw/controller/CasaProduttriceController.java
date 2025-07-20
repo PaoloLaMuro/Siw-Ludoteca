03b986dbf93700ee1fbf95b42b445c7e3cfc2da9
PaoloLaMuro <pao.lamuro@stud.uniroma3.it>
2025-07-20 17:05:16 +0200

    caseProduttrici

---
+++ src/main/java/it/uniroma3/siw/controller/CasaProduttriceController.java
@@ -0,0 +1,24 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.uniroma3.siw.service.CasaProduttriceService;
+import jakarta.persistence.criteria.CriteriaBuilder.Case;
+
+@Controller
+public class CasaProduttriceController {
+
+
+    @Autowired
+    CasaProduttriceService casaProduttriceService;
+
+    @GetMapping("/caseProduttrici")
+    public String getCaseProduttrici(Model model) {
+        model.addAttribute("caseProduttrici", casaProduttriceService.getAllCaseProduttrici());
+        return "caseProduttrici";
+    }
+
+}
